23dc17f623669651b0cfb8d3524cbd6ef28b0c6e
DrSemFrankenstein <DrSemFrankenstein>
2024-09-02 00:04:05 +0300

    pwa

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useEffect } from 'react';
 import "./App.css";
 import AppBar from "./Components/AppBar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -6,12 +7,32 @@ import AboutComponent from "./Components/Profile/AboutComponent";
 import TodoComponent from "./Components/Goals/TodoComponent";
 
 function App() {
+  useEffect(() => {
+    const handleBeforeInstallPrompt = (e) => {
+      e.preventDefault(); // Prevent the mini-infobar from appearing on mobile
+      e.prompt(); // Automatically show the install prompt
+      e.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the install prompt');
+        } else {
+          console.log('User dismissed the install prompt');
+        }
+      });
+    };
+
+    window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
+
+    return () => {
+      window.removeEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
+    };
+  }, []);
+
   return (
     <>
       <AppBar />
       <div className="content">
         <Routes>
-          <Route path="/" element={<Home/>} />
+          <Route path="/" element={<Home />} />
           <Route path="/profile" element={<AboutComponent />} />
           <Route path="/goals" element={<TodoComponent />} />
           <Route path="/settings" element={<h1>Settings</h1>} />
