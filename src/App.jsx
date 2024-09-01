75d9bb595ccc7ad3faa82d5ecdffc8e6dd374a7c
DrSemFrankenstein <DrSemFrankenstein>
2024-09-02 00:16:42 +0300

    basename

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import "./App.css";
 import AppBar from "./Components/AppBar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
 import AboutComponent from "./Components/Profile/AboutComponent";
 import TodoComponent from "./Components/Goals/TodoComponent";
@@ -31,14 +31,12 @@ function App() {
     <>
       <AppBar />
       <div className="content">
-        <Router basename="/achievements">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/profile" element={<AboutComponent />} />
-            <Route path="/goals" element={<TodoComponent />} />
-            <Route path="/settings" element={<h1>Settings</h1>} />
-          </Routes>
-        </Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<AboutComponent />} />
+          <Route path="/goals" element={<TodoComponent />} />
+          <Route path="/settings" element={<h1>Settings</h1>} />
+        </Routes>
       </div>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import "./index.css";
 import { Provider } from "react-redux";
 import store, { persistor } from "./Redux/store.js";
 import { PersistGate } from "redux-persist/lib/integration/react.js";
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <Router>
+        <Router basename="/achievements">
           <App />
         </Router>
       </PersistGate>
