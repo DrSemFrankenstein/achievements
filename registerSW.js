ed7d0402477c9dfae3edfdaa136a6cc294a32be2
DrSemFrankenstein <DrSemFrankenstein>
2024-09-01 23:56:50 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/achievements/sw.js', { scope: '/achievements/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
